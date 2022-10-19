498bae296fd7b5f92b5901f20f3e1fdee8985050
Jean Marcel <jeanvanzella91@gmail.com>
2022-10-19 20:17:45 -0300

    daily

--- trybe.js
+++ trybe.js
@@ -1,20 +1,11 @@
-function isPrime(n) {
-  if (n === 1) {
-    return 1
-  } else if (n === 2) {
-    return 1
-  }
-  let somatorio = 0
-  let newArray = []
-  for (let i = 2; i <= n; i++) {
-    if (n % i === 0) {
-      somatorio += 1
-      newArray.push(i)
+function find_max(nums) {
+  let max_num = 0 // smaller than all other numbers
+  for (let num of nums) {
+    if (num > max_num) {
+      max_num = num;
     }
   }
-  if (somatorio === 1) {
-    return 1
-  } if (somatorio > 1 ) {
-    return newArray[0]
-  };
+  return max_num;
 }
+
+console.log(find_max([1, 2, 6 , 8, 12, 9]))
